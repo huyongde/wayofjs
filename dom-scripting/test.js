4fe6b53a48bd6fe4a2e2d6e3e21407d45a9b5da6
userupgchnxddxtwgh <huyongdeshuxue@126.com>
2016-06-24 16:44:08 +0800

    js 操作css, createElement, createTextNode, appendChild等学习

--- dom-scripting/test.js
+++ dom-scripting/test.js
@@ -15,7 +15,6 @@ function countChildNum(){
 	alert(body_element.length);
 }
 
-window.onload=genLink;
 function genLink(){
 	var elements = document.getElementsByTagName("a"); //此处是getElementsByTagName，不是getElementsByName
 	for (var i = 0; i < elements.length; i++) {
@@ -28,4 +27,42 @@ function genLink(){
     		return false;
     	};
 	}
+}
+function test() {
+	/*
+	var testdiv = document.getElementById("testdiv");
+	var para = document.createElement("p");
+	var txt = document.createTextNode("hello, i am added by js");
+	para.appendChild(txt);
+	testdiv.appendChild(para);
+	*/
+	var para = document.createElement("p");
+	var txt = document.createTextNode("This is ");
+	para.appendChild(txt);
+	var emphasis = document.createElement("em");
+	var txt2 = document.createTextNode("my");
+	emphasis.appendChild(txt2);
+	para.appendChild(emphasis);
+	var txt3 = document.createTextNode(" content!");
+	para.appendChild(txt3);
+	var testdiv = document.getElementById("testdiv");
+	testdiv.style.color = "red";
+	testdiv.style.fontWeight = "bold";
+	testdiv.style.fontsize = "1.2em";
+	testdiv.appendChild(para);
+}
+function dealTable() {
+	var trs = document.getElementsByTagName("tr");
+	var tr_cnt = trs.length;
+	for (var i=0; i<tr_cnt; i++) {
+		if(i%2 == 1){
+			trs[i].style.backgroundColor = "green";
+			trs[i].style.height = "30";
+		}
+	}
+}
+window.onload = function() {
+	test();
+	genLink();
+	dealTable();
 }
