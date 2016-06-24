4b59c12992a210b8fe8ae4e55079a145d0e16139
huyongde <huyongdeshuxue@126.com>
2016-06-24 14:10:19 +0800

    html dom vs dom core

--- dom-scripting/test.js
+++ dom-scripting/test.js
@@ -1,8 +1,31 @@
-var a = 100;
-alert(a);
+function showPic(whichPic){ // dom core
+	var source = whichPic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src", source);
+}
 
-function test() {
-	console.log("test");
-	alert("hello world");
-	document.getElementById("");
+function showPicV2(whichPic){  // html dom
+	var source = whichPic.href;
+	var placeholder = document.getElementById("placeholder");
+	placeholder.src = source;
+}
+
+function countChildNum(){
+	var body_element = document.getElementsByName('body');
+	alert(body_element.length);
+}
+
+window.onload=genLink;
+function genLink(){
+	var elements = document.getElementsByTagName("a"); //此处是getElementsByTagName，不是getElementsByName
+	for (var i = 0; i < elements.length; i++) {
+    	elements[i].onclick = function(){
+    		showPicV2(this); // 此处只能用this，不能用elements[i]
+    		return false;
+    	};
+    	elements[i].onkeypress = function(){
+    		showPicV2(this); // 此处只能用this，不能用elements[i]
+    		return false;
+    	};
+	}
 }
